b8730f05ff1dc5113b89873e0e209e7481898846
Gremious <gremious@protonmail.com>
2019-03-02 21:27:32 +0000

    Written silent json + reorganizing

--- ArchetypeAPI/src/main/java/archetypeAPI/archetypes/theSilent/basicSilent.java
+++ ArchetypeAPI/src/main/java/archetypeAPI/archetypes/theSilent/basicSilent.java
@@ -3,7 +3,7 @@ package archetypeAPI.archetypes.theSilent;
 import archetypeAPI.archetypes.abstractArchetype;
 
 public class basicSilent extends abstractArchetype {
-    private static String archetypeFile = "archetypeAPIResources/localization/eng/archetypes/archetype-Silent-Strings.json";
+    private static String archetypeFile = "archetypeAPIResources/localization/eng/archetypes/theSilent/basic-Silent-Archetype.json";
 
     public basicSilent(boolean useArchetype) {
         super(archetypeFile, useArchetype);
