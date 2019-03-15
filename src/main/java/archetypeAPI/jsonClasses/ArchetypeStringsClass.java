556c0b449ba510e5bba80f81febc33a045c356ce
Kiooeht <kiooeht@gmail.com>
2019-03-14 18:59:55 -0700

    Add character class field to archetypes

--- src/main/java/archetypeAPI/jsonClasses/ArchetypeStringsClass.java
+++ src/main/java/archetypeAPI/jsonClasses/ArchetypeStringsClass.java
@@ -1,6 +1,9 @@
 package archetypeAPI.jsonClasses;
 
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+
 public class ArchetypeStringsClass {
+    public AbstractPlayer.PlayerClass CHARACTER;
     public String NAME;
     public String[] CARD_IDS;
 }
